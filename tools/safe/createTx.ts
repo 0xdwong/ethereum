e004b3cbedc0c037607e77fe34fda4c2708b2fe1
0xdwong <0xdwong@gmail.com>
2024-03-28 15:55:28 +0800

    update: optimise code

--- tools/safe/createTx.ts
+++ tools/safe/createTx.ts
@@ -1,11 +1,11 @@
-import Safe, { EthersAdapter } from '@safe-global/protocol-kit'
-import { MetaTransactionData } from '@safe-global/safe-core-sdk-types'
-import SafeApiKit from '@safe-global/api-kit'
-import { ethers, Wallet, JsonRpcProvider } from 'ethers'
-import { encodeErc20Transfer, encodeErc1155SafeTransferFrom } from './tools'
+import Safe, { EthersAdapter } from '@safe-global/protocol-kit';
+import { MetaTransactionData } from '@safe-global/safe-core-sdk-types';
+import SafeApiKit from '@safe-global/api-kit';
+import { ethers, Wallet, JsonRpcProvider } from 'ethers';
+import { encodeErc20Transfer, encodeErc1155SafeTransferFrom } from './tools';
 
-let dotenv = require('dotenv')
-dotenv.config()
+let dotenv = require('dotenv');
+dotenv.config();
 
 
 async function main() {
@@ -28,25 +28,24 @@ async function main() {
   // build transactions
   const transactions: MetaTransactionData[] = [
     // send ETH
-    // {
-    //   'to': '0x6eAfc7eC94fccd183FADAdB2989394A3369D3f68',
-    //   'data': '0x',
-    //   'value': ethers.parseUnits('0.002', 'ether').toString()
-    // },
+    {
+      'to': '0x6eAfc7eC94fccd183FADAdB2989394A3369D3f68',
+      'data': '0x',
+      'value': ethers.parseUnits('0.002', 'ether').toString()
+    },
 
     // send erc20 tx
-    // {
-    //   'to': '0xD22543c44fb3F33681315E36983338D65B803bA4', // contract address
-    //   'data': encodeErc20Transfer('0x6eAfc7eC94fccd183FADAdB2989394A3369D3f68', 1),
-    //   'value': '0'
-    // },
-
+    {
+      'to': '0xD22543c44fb3F33681315E36983338D65B803bA4', // contract address
+      'data': encodeErc20Transfer('0x6eAfc7eC94fccd183FADAdB2989394A3369D3f68', 1),
+      'value': '0'
+    },
     // send erc1155 NFT (owner, receiver, tokenId, amount)
-    // {
-    //   'to': '0x59412ec9fb7d3a3a6417185df55e2af875e5f311', // NFT contract address
-    //   'data': encodeErc1155SafeTransferFrom('0x5c54B983d42Ef08c663CC3bE63A0Bed2dDF81C7E', '0x6eAfc7eC94fccd183FADAdB2989394A3369D3f68', 1, 2),
-    //   'value': '0'
-    // },
+    {
+      'to': '0x59412ec9fb7d3a3a6417185df55e2af875e5f311', // NFT contract address
+      'data': encodeErc1155SafeTransferFrom('0x5c54B983d42Ef08c663CC3bE63A0Bed2dDF81C7E', '0x6eAfc7eC94fccd183FADAdB2989394A3369D3f68', 1, 2),
+      'value': '0'
+    },
   ]
 
   const safeTransaction = await safeSdk.createTransaction({ transactions });
@@ -55,7 +54,7 @@ async function main() {
 
   // use for calling safe api
   const apiKit = new SafeApiKit({
-    chainId: 11155111n
+    chainId: (await provider.getNetwork()).chainId
   })
 
   await apiKit.proposeTransaction({
